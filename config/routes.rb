785138406a5fb1c2ed94e4d412498b4606c9b8c0
Laura Cárdenas <lacardenasv@unal.edu.co>
2018-04-29 21:50:49 -0500

    :sparkles: ldap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'users/:id', to: "users#show"
   mount_devise_token_auth_for 'User', at: 'auth'
+  resources :ldap
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
